8044eeb360d80a7ec74b6e13c8c85723f7d136f1
ArmandErasmus <armanderas10@gmail.com>
2026-05-09 18:06:04 +0200

    Add files via upload
    Updated auth logic and server setup

---
+++ Smart-event-management-and-ticketing-platform/server.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+
+//views
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret:            process.env.SESSION_SECRET,
+  resave:            false,
+  saveUninitialized: false
+}));
+
+// Make user data available in all views
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
+
+const authRoutes    = require('./routes/authRoutes');
+const eventRoutes   = require('./routes/eventRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
+const enquiryRoutes = require('./routes/enquiryRoutes');
+
+app.use('/auth',     authRoutes);
+app.use('/events',   eventRoutes);
+app.use('/bookings', bookingRoutes);
+app.use('/contact',  enquiryRoutes);
+
+// Home page
+app.get('/', (req, res) => {
+  res.render('index', { events: [], search: '', category: '', date: '' });
+});
+
+// Connect to MongoDB and start server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT || 3000, () =>
+      console.log('Server running on http://localhost:3000')
+    );
+  })
+  .catch(err => console.log('DB connection error:', err));
